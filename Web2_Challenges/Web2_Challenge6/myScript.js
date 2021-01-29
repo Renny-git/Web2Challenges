b40246e4bce3413bde2df20e5e3a828cbb8bef1d
Lauren <laurengibbonz@gmail.com>
2021-01-29 12:35:22 -0800

    LG updates

--- Web2_Challenges/Web2_Challenge6/myScript.js
+++ Web2_Challenges/Web2_Challenge6/myScript.js
@@ -1,4 +1,16 @@
-const b = document.getElementsByClassName("button");
+//Query selector returns the first HTML Element
+const b = document.querySelector(".button");
+const circles = document.querySelectorAll(".circle");
+console.log(circles);
+
+function moveCircles() {
+    circles.forEach(function(elem, index) {
+        elem.classList.toggle('move-circles');
+        elem.style.backgroundColor = color;
+    });
+}
+
 b.onclick = function (){
-    alert("Button clicked!")
+    // alert("Button clicked!")
+    moveCircles;
 };
